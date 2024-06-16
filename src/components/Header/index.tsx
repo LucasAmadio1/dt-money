e96ca15f9ca7681eae11de2c58c5fac4af2d07d2
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2024-06-15 22:24:02 -0300

    visual do app

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,23 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+
+import logoImg from '../../assets/logo.svg'
+import { NewTransectionModal } from '../NewTransactionModal'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransectionModal />
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
